6c3e86f8c6d3cee5514ce13a9e607b5956bf30b1
prabeenragupathi <prabeenprabu@gmail.com>
2025-04-07 00:02:13 +0530

    route added

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from "express";
 import { PORT } from "./config/env.js";
 
+//? routes
+import authRouter from "./routes/auth.routes.js";
+
 const app = express();
+
+//! adding externals routes here
 
 app.get("/", (req, res) => {
   res.send("Welcome to the subcription api");
